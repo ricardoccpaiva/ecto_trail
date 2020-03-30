33001b961bc1a62768887ccc1e05f937403625d9
Ricardo Paiva <ricardoccpaiva@gmail.com>
2020-03-30 11:28:13 +0100

    Update deps

--- mix.exs
+++ mix.exs
@@ -34,8 +34,8 @@ defmodule EctoTrail.Mixfile do
 
   defp deps do
     [
-      {:ecto_sql, "~> 3.0.5"},
-      {:postgrex, "~> 0.14.1", optional: true},
+      {:ecto_sql, ">= 3.0.5"},
+      {:postgrex, ">= 0.14.1", optional: true},
       {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
       {:geo_postgis, "~> 3.1.0", only: [:dev, :test]},
       {:ex_doc, ">= 0.15.0", only: [:dev, :test]},
